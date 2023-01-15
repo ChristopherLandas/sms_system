0098653232093580787ee4998db632fdff2525c5
ChristopherLandas <103894996+ChristopherLandas@users.noreply.github.com>
2023-01-15 20:36:53 +0800

    Add files via upload

---
+++ sqlsetup_queries.sql
@@ -0,0 +1,3 @@
+create database salary_management_system
+#CREATE TABLE login_credentials (usn VARCHAR(64) PRIMARY KEY NOT NULL, pss VARCHAR(128) NOT NULL, slt VARCHAR(64) NOT null)
+#CREATE TABLE account_info (full_name VARCHAR(128) NOT NULL, bday INT(20), salary INT(15), usn VARCHAR(64) PRIMARY KEY REFERENCES login_credentials(usn))
